b972045d7cc2915a81d7f7710cbbb6cedc3b7e47
seungmi lee <71055964+minewet@users.noreply.github.com>
2021-08-19 00:20:49 +0900

    Create boj1922.java

---
+++ boj1922.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+public class boj1922 {
+	
+	public static int n;
+	public static int adj[][];
+	public static boolean contain[];
+	public static int dist[];
+	public static int ans=0;
+	
+	public static void update(int v) {
+		for(int i=1; i<=n; i++) {
+			if(!contain[i]) {
+				if(adj[v][i]!=0 && (dist[i]==-1 || dist[i]>adj[v][i]))
+					dist[i]=adj[v][i];
+			}
+		}
+	}
+	
+	public static int mindist() {
+		int min=0;
+		for(int i=1; i<=n; i++) {
+			if(!contain[i] && dist[i]!=-1) {
+				min= i;
+				break;
+			}
+		}
+		for(int i=min+1;i<=n; i++) {
+			if(!contain[i] && dist[i]!=-1 && dist[i]<dist[min]) 
+				min= i;
+		}
+		return min;
+	}
+	
+	public static void prim(int v) {
+		for(int i=1; i<=n; i++) {
+			dist[i]=-1;
+		}
+		contain[v]=true;
+		update(v);
+		
+		for(int i=1; i<n; i++) {
+			int nxt= mindist();
+			contain[nxt]= true;
+			ans+=dist[nxt];
+			update(nxt);
+		}	
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		adj=new int[n+1][n+1];
+		for(int i=0; i<m; i++) {
+			StringTokenizer st= new StringTokenizer(br.readLine()," ");	
+			int a= Integer.parseInt(st.nextToken());
+			int b= Integer.parseInt(st.nextToken());
+			int c= Integer.parseInt(st.nextToken());
+			adj[a][b]=c;
+			adj[b][a]=c;
+		}
+		
+		contain= new boolean[n+1];
+		dist= new int[n+1];
+		
+		prim(1);
+		System.out.println(ans);
+	}
+}
